2b87526cf6a5587c47e7725140a36039fff69d7e
Donistr <deniselag@mail.ru>
2025-12-09 20:27:42 +0400

    расписание

--- shared/src/main/java/org/example/shared/service/impl/ScheduleStorageServiceImpl.java
+++ shared/src/main/java/org/example/shared/service/impl/ScheduleStorageServiceImpl.java
@@ -19,7 +19,7 @@ public class ScheduleStorageServiceImpl implements ScheduleStorageService {
 
     @PostConstruct
     private void init() {
-        LocalDateTime startTime = LocalDateTime.of(2025, 12, 9, 19, 55, 0);
+        LocalDateTime startTime = LocalDateTime.of(2025, 12, 9, 20, 28, 0);
         nameToSchedule.put("1", new ScheduleDto(
                 startTime,
                 startTime.plusMinutes(5),
