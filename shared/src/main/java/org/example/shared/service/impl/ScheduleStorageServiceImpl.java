1556251e6d16143a7f6a4891f453404ff9ac155a
Donistr <deniselag@mail.ru>
2025-12-09 15:20:25 +0400

    fix: изменил ScheduleStorageServiceImpl так, чтобы он отдавал изменяемые списки

--- shared/src/main/java/org/example/shared/service/impl/ScheduleStorageServiceImpl.java
+++ shared/src/main/java/org/example/shared/service/impl/ScheduleStorageServiceImpl.java
@@ -7,6 +7,7 @@ import org.example.shared.service.ScheduleStorageService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,22 +23,22 @@ public class ScheduleStorageServiceImpl implements ScheduleStorageService {
         nameToSchedule.put("1", new ScheduleDto(
                 time,
                 time.plusMinutes(5),
-                List.of(
+                new ArrayList<>(List.of(
                         new TimePeriodDto(time.plusSeconds(30), time.plusMinutes(1)),
                         new TimePeriodDto(time.plusMinutes(1).plusSeconds(50), time.plusMinutes(2).plusSeconds(10)),
                         new TimePeriodDto(time.plusMinutes(3), time.plusMinutes(3).plusSeconds(45)),
                         new TimePeriodDto(time.plusMinutes(4), time.plusMinutes(4).plusSeconds(20))
                 )
-        ));
+        )));
         nameToSchedule.put("2", new ScheduleDto(
                 time,
                 time.plusMinutes(3),
-                List.of(
+                new ArrayList<>(List.of(
                         new TimePeriodDto(time.plusSeconds(30), time.plusMinutes(1)),
                         new TimePeriodDto(time.plusMinutes(1).plusSeconds(15), time.plusMinutes(1).plusSeconds(30)),
                         new TimePeriodDto(time.plusMinutes(2), time.plusMinutes(2).plusSeconds(10))
                 )
-        ));
+        )));
         System.out.println(nameToSchedule);
     }
 
